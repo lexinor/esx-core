3fd21f622bb44b267def43bf13490c1880f694c9
Lexinor <alternonolimit@gmail.com>
2022-11-14 19:09:25 +0100

    Small config changes

--- [esx_addons]/esx_policejob/config.lua
+++ [esx_addons]/esx_policejob/config.lua
@@ -6,21 +6,21 @@ Config.MarkerSize                 = {x = 1.5, y = 1.5, z = 0.5}
 Config.MarkerColor                = {r = 50, g = 50, b = 204}
 
 Config.EnablePlayerManagement     = true -- Enable if you want society managing.
-Config.EnableArmoryManagement     = false
+Config.EnableArmoryManagement     = true
 Config.EnableESXIdentity          = true -- Enable if you're using esx_identity.
-Config.EnableESXOptionalneeds     = false -- Enable if you're using esx_optionalneeds
-Config.EnableLicenses             = false -- Enable if you're using esx_license.
+Config.EnableESXOptionalneeds     = true -- Enable if you're using esx_optionalneeds
+Config.EnableLicenses             = true -- Enable if you're using esx_license.
 
 Config.EnableHandcuffTimer        = true -- Enable handcuff timer? will unrestrain player after the time ends.
 Config.HandcuffTimer              = 10 * 60000 -- 10 minutes.
 
-Config.EnableJobBlip              = false -- Enable blips for cops on duty, requires esx_society.
+Config.EnableJobBlip              = true -- Enable blips for cops on duty, requires esx_society.
 Config.EnableCustomPeds           = false -- Enable custom peds in cloak room? See Config.CustomPeds below to customize peds.
 
-Config.EnableESXService           = false -- Enable esx service?
-Config.MaxInService               = -1 -- How many people can be in service at once? Set as -1 to have no limit
+Config.EnableESXService           = true -- Enable esx service?
+Config.MaxInService               = 50 -- How many people can be in service at once? Set as -1 to have no limit
 
-Config.Locale = GetConvar('esx:locale', 'en')
+Config.Locale = GetConvar('esx:locale', 'fr')
 
 Config.OxInventory                = ESX.GetConfig().OxInventory
 
